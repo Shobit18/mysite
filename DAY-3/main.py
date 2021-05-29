37e10aa36af3114bc8a2bcc0dea02c902ca7e55f
Shobit Kunwar <84620303+Shobit18@users.noreply.github.com>
2021-05-29 15:10:19 +0530

    Day-3
    day 3 internship program

---
+++ DAY-3/main.py
@@ -0,0 +1,113 @@
+# user input
+# name = input("Enter name")
+
+# num1 = int(input("Enter number"))
+# num2 = int(input("Enter number"))
+
+# ans = n1 + n2
+
+# print("sum is", ans)
+
+
+
+# Conditional statement
+
+# if statement
+# n1 = 10
+# n2 = 20
+# if n1>n2:
+#     print("n1 is grater..")
+
+# if n2>n1:
+#     print("n2 is grater..")
+
+
+
+# if else statement
+
+# n1 = 10
+# n2 = 20
+
+# if n1>n2:
+#     print("n1 is greater..")
+
+# else:
+#     print("n2 is greater..")
+
+# elseif statement
+
+# n1 = 10
+# n2 = 20
+
+
+# if n1 == n2:
+#     print("n1 and n2 are same")
+
+# elif n1>n2:
+#     print("n1 is greater..")
+
+# else:
+#     print("n2 is greater..")
+
+# n1 = 10
+
+# if n1>=0:
+#     if n1 == 0:
+#         print("n1 is zero...")
+#     else:
+#         print("n1 is positive")
+
+# else:
+#     print("n1 is negative..")
+
+# loop in python
+
+# i = 1
+# while(i<5):
+#     print("value is ",i)
+#     i += 1
+# print(i)
+
+
+# for loop
+
+# for i in 'Hello':
+#     print("Value : ",i)
+
+# l1 = [10, 20, 'hello']
+# print(l1)
+
+# for i in l1:
+#     print("value is", i)
+
+
+# range funtion
+# for i in range(10):
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+# len function
+# list1 = [10, 20, "Akash Technolabs"]
+
+# for i in range(len(list1)):
+#     print(i)
+
+# break and continue
+# l1 = [1,2,3,4,5,6,7,8,9,10]
+
+# for x in range(10):
+#     if x%2 ==5:
+#         break
+#     print(x)    
+
+# for x in range(10):
+#     if x%2 ==0:
+#         continue
+#     print(x) 
+
+for x in range(10):
+    if x%2 ==0:
+        pass
+print("it pass function")
